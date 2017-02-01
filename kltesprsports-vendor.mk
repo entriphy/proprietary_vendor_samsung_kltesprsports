07edb3e039fa1df3b09db036289a80e17394f1ca
evilarceus <evilarceus@gmail.com>
2017-01-31 20:48:31 -0800

    Remove blobs that are included in klte-common

--- kltesprsports-vendor.mk
+++ kltesprsports-vendor.mk
@@ -22,15 +22,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kltesprsports/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/samsung/kltesprsports/proprietary/bin/rild:system/bin/rild \
     vendor/samsung/kltesprsports/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-    vendor/samsung/kltesprsports/proprietary/etc/clatd.conf:system/etc/clatd.conf \
-    vendor/samsung/kltesprsports/proprietary/etc/gps.conf:system/etc/gps.conf \
-    vendor/samsung/kltesprsports/proprietary/etc/izat.conf:system/etc/izat.conf \
-    vendor/samsung/kltesprsports/proprietary/etc/sap.conf:system/etc/sap.conf \
-    vendor/samsung/kltesprsports/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
-    vendor/samsung/kltesprsports/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
-    vendor/samsung/kltesprsports/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
-    vendor/samsung/kltesprsports/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
-    vendor/samsung/kltesprsports/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
     vendor/samsung/kltesprsports/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/kltesprsports/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/kltesprsports/proprietary/lib/libril.so:system/lib/libril.so \
@@ -41,15 +32,10 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kltesprsports/proprietary/vendor/firmware/bcm2079xB4_pre_firmware.ncd:system/vendor/firmware/bcm2079xB4_pre_firmware.ncd \
     vendor/samsung/kltesprsports/proprietary/vendor/firmware/bcm2079xB5_firmware.ncd:system/vendor/firmware/bcm2079xB5_firmware.ncd \
     vendor/samsung/kltesprsports/proprietary/vendor/firmware/bcm2079xB5_pre_firmware.ncd:system/vendor/firmware/bcm2079xB5_pre_firmware.ncd \
-    vendor/samsung/kltesprsports/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
-    vendor/samsung/kltesprsports/proprietary/vendor/lib/libflp.so:system/vendor/lib/libflp.so \
-    vendor/samsung/kltesprsports/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
-    vendor/samsung/kltesprsports/proprietary/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
-    vendor/samsung/kltesprsports/proprietary/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
     vendor/samsung/kltesprsports/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
